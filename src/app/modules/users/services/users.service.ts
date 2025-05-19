504a25d47b51c7943d29fdaab6d4cc064c08e959
Adrian Amaris <aleoamaris@gmail.com>
2025-05-19 16:36:47 -0500

    user register completed

--- src/app/modules/users/services/users.service.ts
+++ src/app/modules/users/services/users.service.ts
@@ -1,7 +1,8 @@
 // src/app/modules/users/services/user.service.ts
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient,HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { throwError } from 'rxjs';
 
 export interface User {
   nombre: string;
@@ -20,6 +21,19 @@ export class UserService {
   constructor(private http: HttpClient) {}
 
   register(user: User): Observable<User> {
+    // console.log(user)
     return this.http.post<User>(`${this.apiUrl}/register`, user);
+  }
+  private handleError(error: HttpErrorResponse) {
+    let errorMessage = 'Error desconocido';
+    
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      errorMessage = error.error?.error || error.message;
+    }
+    
+    console.error(error);
+    return throwError(() => new Error(errorMessage));
   }
 }
